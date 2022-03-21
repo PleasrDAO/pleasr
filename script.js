38323f15e7d269568e51dbe90bf6ec7179183d4c
Spencer Harrison <spencerjharrison@gmail.com>
2022-03-21 12:00:54 -0400

    fixed syntax

--- script.js
+++ script.js
@@ -127,14 +127,14 @@ const itemProps = [
     title: 'Once Upon a Time in Shaolin',
     link: 'https://pleasr.mirror.xyz/PTzSIYe6LbNW55i_Jo4S_fgqIiDp3d7YblpikQ1iRks',
     by: 'Wu-Tang Clan'
-  }
+  },
   {
     src: 'img/wikipedia.jpg',
     aspect: 1,
     title: 'The Birth of Wikipedia',
     link: 'https://www.christies.com/features/first-wikipedia-edit-to-be-sold-as-nft-11983-1.aspx',
     by: 'Jimmy Wales'
-  }
+  },
   {
     src: 'img/car-remote.jpg',
     aspect: 1,
